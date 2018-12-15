65d86561948fc2c44b8623f259322502f699ee2a
michael_townsend <michael.h.townsend@gmail.com>
2018-12-14 20:31:36 -0500

    Added Previously crossed over

--- CreateBollingerBands.py
+++ CreateBollingerBands.py
@@ -16,25 +16,40 @@ import pandas as pd
 
 def main():
     inputPath = "C:\\Users\\Michael Townsend\\Desktop\\StockInfo\\FOREX\\Data\\DAT_ASCII_USDJPY_M1_2017_TRIMMED.csv"
-    outputPath = "C:\\Users\\Michael Townsend\\Desktop\\StockInfo\\FOREX\\Data\\BollingerBands.txt"
+    outputPath = "C:\\Users\\Michael Townsend\\Desktop\\StockInfo\\FOREX\\Data\\BollingerBands.csv"
+    stdDeviations = 3
+    rollingWindow = 50
     inputDataFrame = pd.read_csv(inputPath, delimiter=';')
-    inputDataFrame['20 Min MA'] = inputDataFrame['BarCloseBid'].rolling(window=20).mean()
-    inputDataFrame['20 Min STD'] = inputDataFrame['BarCloseBid'].rolling(window=20).std()
-    inputDataFrame['UpperBand'] = inputDataFrame['20 Min MA'] + (inputDataFrame['20 Min STD'] * 2)
-    inputDataFrame['LowerBand'] = inputDataFrame['20 Min MA'] - (inputDataFrame['20 Min STD'] * 2)
+    inputDataFrame['20 Min MA'] = inputDataFrame['BarCloseBid'].rolling(window=rollingWindow).mean()
+    inputDataFrame['20 Min STD'] = inputDataFrame['BarCloseBid'].rolling(window=rollingWindow).std()
+    inputDataFrame['UpperBand'] = inputDataFrame['20 Min MA'] + (inputDataFrame['20 Min STD'] * stdDeviations)
+    inputDataFrame['LowerBand'] = inputDataFrame['20 Min MA'] - (inputDataFrame['20 Min STD'] * stdDeviations)
     
     # Crossed above over Upper Bollinger Band
-    inputDataFrame['CrossedUpper'] = inputDataFrame.where(inputDataFrame['BarCloseBid'] > inputDataFrame['UpperBand'], False, False).any(axis=1)
+    inputDataFrame['CrossedUpper'] = inputDataFrame.where(inputDataFrame['BarCloseBid'] > inputDataFrame['UpperBand'], False, False).any(axis=1).astype(int)
     
     # Crossed under over Lower Bollinger Band
-    inputDataFrame['CrossedLower'] = inputDataFrame.where(inputDataFrame['BarCloseBid'] < inputDataFrame['LowerBand'], False, False).any(axis=1)
+    inputDataFrame['CrossedLower'] = inputDataFrame.where(inputDataFrame['BarCloseBid'] < inputDataFrame['LowerBand'], False, False).any(axis=1).astype(int)
+    
+    # Slope
+    inputDataFrame['Slope'] = (inputDataFrame['BarCloseBid'] - inputDataFrame['BarCloseBid'].shift(1)) / 5
+    
+    # Crossed Upper Bound in the previous time interval (speculating that what goes up must come down, hopefully in this time interval)
+    inputDataFrame['PreviouslyCrossedUpper'] = inputDataFrame.where(inputDataFrame['CrossedUpper'].shift(1) == True, False, False).any(axis = 1).astype(int)
     
     # a win occurrs if a given row's BarCloseBid is less then the previous row's value
-    inputDataFrame['SellWins'] = inputDataFrame.where(inputDataFrame['BarCloseBid'] >= inputDataFrame['BarCloseBid'].shift(-1), False, False).any(axis=1)
+    inputDataFrame['SellWins'] = inputDataFrame.where(inputDataFrame['BarCloseBid'] >= inputDataFrame['BarCloseBid'].shift(-1), False, False).any(axis=1).astype(int)
+    
+    # filter data
+    previouslyCrossedUpper = inputDataFrame['PreviouslyCrossedUpper'] == True
+    negativeSlope = inputDataFrame['Slope'] < -0.004
     
     # print out analyzed data
-    header = ['DateTimeStamp','BarOpenBid','BarHighBid','BarLowBid','BarCloseBid','Volume','20 Min MA', '20 Min STD', 'UpperBand', 'LowerBand', 'CrossedUpper', 'CrossedLower', 'SellWins']
-    inputDataFrame.to_csv(outputPath, columns = header)
+    #header = ['CrossedUpper', 'Slope', 'PreviouslyCrossedUpper', 'SellWins']
+    header = ['DateTimeStamp','BarOpenBid','BarHighBid','BarLowBid','BarCloseBid','Volume','20 Min MA', '20 Min STD', 'UpperBand', 'LowerBand', 'CrossedUpper', 'CrossedLower', 'Slope', 'PreviouslyCrossedUpper','SellWins']
+    
+    inputDataFrame[previouslyCrossedUpper & negativeSlope].to_csv(outputPath, columns = header)
+    #inputDataFrame.to_csv(outputPath, columns = header)
     #print("Exiting")
     
 main()
